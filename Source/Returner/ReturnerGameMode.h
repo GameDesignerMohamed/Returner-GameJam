29934c229cc7c27dc7db8c1393a5f58e72e50c21
Mohamed Abdel Khalik <gamedesignermohamed@gmail.com>
2024-10-25 21:44:41 +0300

    Default Third Person

---
+++ Source/Returner/ReturnerGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ReturnerGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AReturnerGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AReturnerGameMode();
+};
+
+
+
